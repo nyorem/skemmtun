771e51cba543e322dbf8b3929ffcf372a430c240
Jocelyn MEYRON <Jocelyn.Meyron@grenoble-inp.org>
2015-11-29 18:25:11 +0100

    Check credentials

--- src/Main.hs
+++ src/Main.hs
@@ -2,11 +2,17 @@
 
 module Main where
 
+import Control.Exception
 import Control.Lens ( (&), (^.), (?~) )
+import Control.Monad ( void )
 import qualified Data.ByteString.Lazy as B
+import Data.Function ( on )
+import Data.List
+import Data.Ord
 import Data.String
 import qualified Data.Text as T
 import qualified Data.Text.Read as T
+import Network.HTTP.Client ( HttpException(..) )
 import Network.Wreq
 import Text.XML
 import Text.XML.Cursor
@@ -27,27 +33,30 @@ readCredentials file = do
     contents <- Prelude.readFile file
     let ls = lines contents
         (u, p) = (head ls, head . tail $ ls)
-        extract s = head $ tail $ words $ snd $ span (/= '=') s
+        extract = head . tail . words . snd . span (/= '=')
     return $ (extract u, extract p)
 
+-- Authentification credentials
 opts :: Credentials -> Options
 opts (username, password) =
     defaults & auth ?~ basicAuth (fromString username) (fromString password)
 
-verifyCredentials :: Credentials -> IO (Response B.ByteString)
-verifyCredentials creds =
-    getWith (opts creds) "http://myanimelist.net/api/account/verify_credentials.xml"
+-- Check if the credentials are correct
+verifyCredentials :: Credentials -> IO ()
+verifyCredentials creds = do
+    void $ getWith (opts creds) "http://myanimelist.net/api/account/verify_credentials.xml" `catch` handler
+    where handler (StatusCodeException _ _ _) = error "Credentials verification failed!"
 
 listAll :: Credentials -> Name -> String -> IO [(T.Text, Maybe MyStatus)]
 listAll creds ty uname = do
     let url = concat ["http://myanimelist.net/malappinfo.php?u=", uname, "&status=all&type=", T.unpack $ nameLocalName ty ]
         opts' = opts creds
     r <- getWith opts' url
-    let cur = fromDocument $ parseLBS_ def $ r ^. responseBody
-        titles = cur $| element "myanimelist" &/ element ty &/ element "series_title" &/ content
-        statuses' = cur $| element "myanimelist" &/ element ty &/ element "my_status" &/ content
+    let cur      = fromDocument $ parseLBS_ def $ r ^. responseBody
+        titles   = cur $| element "myanimelist" &/ element ty &/ element "series_title" &/ content
+        statuses'= cur $| element "myanimelist" &/ element ty &/ element "my_status" &/ content
         statuses = map (\s -> either (error "Error during status parsing") (toMyStatus . fst) $ T.decimal s) statuses'
-    return $ zip titles statuses
+    return $ sortBy (comparing snd) $ zip titles statuses
 
 animeList :: Credentials -> String -> IO [(T.Text, Maybe MyStatus)]
 animeList creds =
@@ -62,7 +71,7 @@ data MyStatus = Current
               | OnHold
               | Dropped
               | Planned
-              deriving (Show)
+              deriving (Eq, Ord, Show)
 
 toMyStatus :: Int -> Maybe MyStatus
 toMyStatus 1 = Just Current
@@ -72,9 +81,14 @@ toMyStatus 4 = Just Dropped
 toMyStatus 6 = Just Planned
 toMyStatus _ = Nothing
 
+organize :: [(T.Text, Maybe MyStatus)] -> [(Maybe MyStatus, [T.Text])]
+organize =
+    map (\xs -> (snd . head $ xs, map fst xs)) . groupBy ((==) `on` snd)
+
 main :: IO ()
 main = do
     creds <- readCredentials credentialsFile
-    animeList creds "nyorem" >>= print
-    mangaList creds "nyorem" >>= print
+    verifyCredentials creds
+    animeList creds "nyorem" >>= print . organize
+    mangaList creds "nyorem" >>= print . organize
 
